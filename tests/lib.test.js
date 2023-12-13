e801794c9f66c7e9997dc357f08206f5a5ceac71
jass <jasbrar080@gmail.com>
2023-12-12 21:19:40 -0800

    testing numbers

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,3 +1,16 @@
-test("our first test",()=>{
-throw new Error("something failed");
+const lib=require('../lib');
+test("Result should be positive number if input is positive number",()=>{
+const result=lib.absolute(1);
+expect(result).toBe(1);
 });
+
+test("Result should be positive number if input is negative number",()=>{
+  const result=lib.absolute(-1);
+  expect(result).toBe(1);
+  });
+
+test("Result should be 0 if input is 0",()=>{
+  const result=lib.absolute(0);
+  expect(result).toBe(0);
+ });
+    
