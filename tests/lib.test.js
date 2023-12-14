3d85fc8cb97e64891165af8973f607563ebf080b
jass <jasbrar080@gmail.com>
2023-12-13 20:04:19 -0800

    Grouping Tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,16 +1,17 @@
 const lib=require('../lib');
-test("Result should be positive number if input is positive number",()=>{
-const result=lib.absolute(1);
-expect(result).toBe(1);
-});
-
-test("Result should be positive number if input is negative number",()=>{
-  const result=lib.absolute(-1);
-  expect(result).toBe(1);
-  });
-
-test("Result should be 0 if input is 0",()=>{
-  const result=lib.absolute(0);
-  expect(result).toBe(0);
- });
+describe('absulate',()=>{
+  it("should be positive number if input is positive number",()=>{
+    const result=lib.absolute(1);
+    expect(result).toBe(1);
+    });
     
+    it("should be positive number if input is negative number",()=>{
+      const result=lib.absolute(-1);
+      expect(result).toBe(1);
+      });
+    
+    it("should be 0 if input is 0",()=>{
+      const result=lib.absolute(0);
+      expect(result).toBe(0);
+     });      
+});    
